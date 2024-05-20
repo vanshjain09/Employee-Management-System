c99cd86a4810f98132bd51ea12297fe942e66fac
vanshjain09 <vanshjain499@gmail.com>
2024-05-20 12:54:34 +0530

    React Code Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes ,Route} from 'react-router-dom';
+import EmployeeList from './components/EmployeeList';
+import UpdateEmployee from './components/UpdateEmployee';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    <Route index element={<EmployeeList/>}/>
+    <Route path='/addEmployee' element={<AddEmployee/>}/>
+    <Route path='/' element={<EmployeeList/>}/>
+    <Route path='/employeeList' element={<EmployeeList/>}/>
+    <Route path='/editEmployee/:id' element={<UpdateEmployee/>}/>
+  </Routes>
+  </BrowserRouter>
+  </>;
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+          <div className="bg-gray-800 h-16 flex items-center">
+            <p className='text-white font-semibold'>Employee Management System</p>
+          </div>
+        </div>
+      );
+}
+
+export default Navbar
